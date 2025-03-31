f99ceb0548d5fb1933ae586512e4ebada09f4831
Faf-dev <noaillesjulien@gmail.com>
2025-03-31 22:25:38 +0200

    push all empty files + T0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,11 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    const succes = true;
+
+    if (succes) {
     resolve('true');
+    } else {
+      reject('false');
+    }
   });
 }
