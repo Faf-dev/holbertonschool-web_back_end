d52bd6682f1002fe0205ea26c9aa6d2bee75a601
Faf-dev <noaillesjulien@gmail.com>
2025-04-01 17:17:00 +0200

    fix eslint

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    queue.push(mathFunction())
+    queue.push(mathFunction());
   } catch (error) {
-    queue.push(error.message)
+    queue.push(error.message);
   }
-  queue.push('Guardrail was processed')
+  queue.push('Guardrail was processed');
   return queue;
 }
