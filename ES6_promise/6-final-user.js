7d420882800938f09bba3c9c358fa41d76f40f53
Faf-dev <noaillesjulien@gmail.com>
2025-04-01 17:23:56 +0200

    fix eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,12 +3,10 @@ import uploadPhoto from '5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-    return results.map((result) => {
+    .then((results) => results.map((result) => {
       if (result.status === 'fulfilled') {
         return { status: result.status, value: result.value };
       }
-      return { status: result.status , value: result.reason };
-    });
-  });
+      return { status: result.status, value: result.reason };
+    }));
 }
