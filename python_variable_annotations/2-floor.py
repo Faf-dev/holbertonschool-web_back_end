bf9ddd9b47194a4e4e18d40dadf7eb23a8b49999
Faf-dev <noaillesjulien@gmail.com>
2025-04-22 16:49:25 +0200

    push all task

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def floor(n: float) -> int:
+    """
+    Return the largest integer less than or equal to n.
+    """
+    if n >= 0:
+        return int(n)
+    else:
+        return int(n) - 1
