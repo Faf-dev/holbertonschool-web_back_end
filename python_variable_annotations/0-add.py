bffb0892feb22cc49de9ba50ca028d302001740f
Faf-dev <noaillesjulien@gmail.com>
2025-04-22 16:52:29 +0200

    fix docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-0-add.py
+0-add.py is a function that add two floats together.
+It takes two float arguments and returns their sum as a float.
 """
 
 def add(a: float, b: float) -> float:
